f528b583b79a94d815ee7076423f78770022b8eb
ifarra1324 <ifarra1324@gmail.com>
2024-10-27 22:06:57 +0000

    API

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fasthtml.common import *
+from PIL import Image, ImageDraw, ImageFont
+import os
 
 app,rt = fast_app()
 
@@ -33,5 +35,40 @@ mainsc = Div(
 
 @rt('/')
 def get(): return Html(*headers, mainsc)
+
+# Path to the certificate template
+TEMPLATE_PATH = "certificate_template.png"
+
+def create_certificate(name):
+    certificate = Image.open(TEMPLATE_PATH)
+    draw = ImageDraw.Draw(certificate)
+
+    font_path = "arial.ttf" 
+    font_size = 70
+    font = ImageFont.truetype(font_path, font_size)
+
+    text_bbox = draw.textbbox((0, 0), name, font=font)
+    text_width = text_bbox[2] - text_bbox[0]
+    text_height = text_bbox[3] - text_bbox[1]
+    
+    text_x = (certificate.width - text_width) // 2
+    text_y = (certificate.height - text_height + 310) // 2
+
+    draw.text((text_x, text_y), name, fill="black", font=font)
+
+    output_path = f"certificate/{name}_certificate.png"
+    certificate.save(output_path)
+    return output_path
+
+@app.route("/generate/{name}", methods=['GET'])
+def generate_certificate(name: str):
+    if not name:
+        return "Name parameter is required", 400
+    
+    certificate_path = create_certificate(name)
+    return FileResponse(f'{certificate_path}', filename=f'{name}_certificate.png', headers={"Content-Disposition": f"attachment; filename={name}_certificate.png"})
+    # return FileResponse(f'{certificate_path}')
+
+
 
 serve()
